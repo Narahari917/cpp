3b74418ea6c2d8db2c2768d8f54d60cde1bc24ff
hari kommi <naraharikommi644@gmail.com>
2023-01-13 19:37:19 +0530

    singly linked list added

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,26 +1,61 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 class Node {
     public:
     int data;
     Node* next;
-    Node() {
-        data = 0;
-        next = nullptr;
     Node(int data){
         this->data = data;
         this->next = nullptr;
     }
-    Node(int data, Node* next){
-            this->data = data;
-            this->next = next;
-    
-}
+    Node(){
+        this->data=0;
+        this->next=nullptr;
+    }
+    Node(int data,Node* next){
+        this->data = data;
+        this->next = next;
+    }
+};
 class linkedlist{
-    public:
     Node* head;
-    linkedlist() {
-}
-int main(int argc, char){
+    Node* tail;
+    public:
+    linkedlist(){
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+     void insert_at_front(int data){
+        Node* n = new Node(data);
+
+        if(head == nullptr){
+            head = n;
+            tail = n;
+            return;
+        }
+
+        n->next = head;
+        head = n;
+    }
+    void print(){
+      Node* temp = head;
+        while(temp){
+
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        
+    }
+    
+};
+int main(){
+    linkedlist ll;
+    Node* root = new Node;
+    
+    ll.insert_at_front(2);
+    ll.insert_at_front(3);
+    ll.insert_at_front(4);
+    ll.print();
+    
     return 0;
 }
